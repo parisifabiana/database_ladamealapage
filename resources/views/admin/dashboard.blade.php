6994ad895ceb218b2dcc14be3028913fd0ff4bd4
Fabiana Parisi <parisifabiana@gmail.com>
2024-07-09 22:12:49 +0200

    pagina dashboard added

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,16 +2,39 @@
 
 @section('content')
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Dashboard</title>
-</head>
-<body>
-    <h2>Parte amministrativa - Dashboard</h2>
-</body>
-</html>
+<div class="container my-5">
+    <div class="row justify-content-center">
+        <div class="col-12 col-md-6">
+            <form method="POST" action="{{ route('admin.dashboard') }}">
+            @csrf
+                <div class="mb-3">
+                <label for="exampleInputNome" class="form-label">Nome</label>
+                <input type="text" name="name" class="form-control" id="exampleInputNome"
+                aria-describedby="nameHelp" required>
+                </div>
+
+                <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" name="email" class="form-control" id="exampleInputEmail1"
+                aria-describedby="emailHelp" required>
+                </div>
+
+                <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" name="password" class="form-control"
+                id="exampleInputPassword1" required>
+                </div>
+
+                <div class="mb-3">
+                <label for="exampleInputPassword2" class="form-label">Confirm Password</label>
+                <input type="password" name="password_confirmation" class="form-control"
+                id="exampleInputPassword2" required>
+                </div>
+
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    </div>
+</div>
+
 @endsection
